05fa9331d355f820bd488d504dbe1b804ab6b178
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-11-20 12:58:18 +0000

    Task3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 -- Grant all privileges to user only on the new database
 -- Grant select privilege to user on perfomance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Ajibosesemiat12@';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev_pwd@localhost;
