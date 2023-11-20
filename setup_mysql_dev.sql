2ed31fcf41eb8a460b12dbfbad1eeb35bba31f4f
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-11-20 13:22:21 +0000

    Task3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,7 @@
 -- Grant all privileges to user only on the new database
 -- Grant select privilege to user on perfomance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev_pwd@localhost;
-GRANT SELECT ON performance_schema TO hbnb_dev_pwd@localhost;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev_pwd@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_dev_pwd@localhost;
 
