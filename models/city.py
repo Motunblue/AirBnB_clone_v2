80448679a0f0301ea67bd4eaec73a02f6399ccc3
Motun <ajirotutu4real@gmail.com>
2023-11-22 22:02:16 +0100

    Modified place class

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -9,3 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship("Place", cascade="all, delete-orphan", backref="cities")
