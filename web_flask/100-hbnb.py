dd2d4a58767aabbfa88f399fd0e67f8908dc0e68
Motun <ajirotutu4real@gmail.com>
2023-12-29 19:32:03 +0100

    Added tak 11

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,16 +4,19 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
     """Render hbnb page"""
 
-    return render_template('10-hbnb_filters.html', states=storage.all(State),
-                           amenities=storage.all(Amenity))
+    return render_template('100-hbnb.html', states=storage.all(State),
+                           amenities=storage.all(Amenity),
+                           places=storage.all(Place))
 
 
 @app.teardown_appcontext
